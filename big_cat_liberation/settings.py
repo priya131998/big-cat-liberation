cc52a6c3ac77a5814b7916005953c35844ea03eb
Lester Leong Jr <lesterleongjr@gmail.com>
2021-08-19 14:00:22 -0700

    configures postgreql, main_app

--- big_cat_liberation/settings.py
+++ big_cat_liberation/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'big_cat_liberation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
