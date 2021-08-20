3c4b7fbb0c363cab5c82ca8f8d02f43feda329d4
Lester Leong Jr <lesterleongjr@gmail.com>
2021-08-20 12:41:09 -0700

    adds create functionality for Cat

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('cats/', views.cats_index, name='index'),
+    path('cats/create/', views.CatCreate.as_view(), name='cat_create'),
     path('accounts/signup/', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Cat
 
-# Create your views here.
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    fields = ['name', 'breed', 'sex', 'age', 'health', 'description', 'image_url']
+    success_url = '/cats/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'sex', 'age', 'health', 'description', 'image_url']
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
 def home (request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', { 'cats': cats })
 
 def signup(request):
     error_message = ''
