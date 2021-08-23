120ae8d22c0fa2d48b6424725156eb6e2b309828
priya131998 <priya.magiya@gmail.com>
2021-08-23 15:10:54 -0700

    create a route for the user

--- big_cat_liberation/settings.py
+++ big_cat_liberation/settings.py
@@ -125,6 +125,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/about/' # Change to '/cats/'
+LOGIN_REDIRECT_URL = '/user/' # Change to '/cats/'
 LOGOUT_REDIRECT_URL = '/'
 
